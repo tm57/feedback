9ac1bba635586cb54cfac34332f57f660d67ecc1
tm57 <tmatate.act@gmail.com>
2015-10-12 20:09:29 +0200

    die after error

--- pre_process.php
+++ pre_process.php
@@ -9,9 +9,9 @@ function reject_blank_form(){
     }
   }
   if ($field_set < 80) {
-    echo "Submission has been rejected because responses submitted are too few<br>
+    die("Submission has been rejected because responses submitted are too few<br>
     Please may you complete at least 80 questions so that we can make meaningful conclusions of the research
-    <br>Thank you";
+    <br>Thank you<br><a href='feedback_form.php'>Form<a>");
   }
 }
 reject_blank_form();
